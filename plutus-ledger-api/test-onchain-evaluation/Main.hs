49f629c7864d58a190996abd35e6908ac299c02c
Ziyang Liu <unsafeFixIO@gmail.com>
2022-06-15 16:55:29 -0700

    wip

--- plutus-ledger-api/test-onchain-evaluation/Main.hs
+++ plutus-ledger-api/test-onchain-evaluation/Main.hs
@@ -1,121 +1,95 @@
-{-# LANGUAGE QuasiQuotes      #-}
-{-# LANGUAGE RecordWildCards  #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Main (main) where
+
+import Plutus.Script.Evaluation.Test.Options qualified as O
+import PlutusCore.Evaluation.Machine.CostModelInterface (CostModelApplyError)
+import PlutusLedgerApi.Common
+import PlutusLedgerApi.Test.EvaluationEvent
+import PlutusLedgerApi.V1 qualified as V1
+import PlutusLedgerApi.V2 qualified as V2
 
 import Codec.Serialise qualified as CBOR
 import Control.Concurrent.ParallelIO.Local qualified as Concurrent
 import Control.Exception (evaluate)
+import Control.Monad.Extra (whenJust)
 import Data.ByteString.Base16 qualified as B16
 import Data.ByteString.Lazy qualified as Lazy
-import Data.Foldable (for_)
 import Data.List (intercalate)
-import Data.Maybe (catMaybes)
+import Data.List.NonEmpty (NonEmpty, nonEmpty, toList)
+import Data.Maybe
 import Data.Text (Text)
 import Data.Text.Encoding qualified as Text
+import Data.Traversable (for)
 import Options.Applicative qualified as OA
-import Plutus.Script.Evaluation.Test.Options qualified as O
-import PlutusCore.Evaluation.Machine.CostModelInterface (CostModelApplyError)
-import PlutusLedgerApi.Test.EvaluationEvent (ScriptEvaluationData (..), ScriptEvaluationEvent (..),
-                                             ScriptEvaluationResult (..))
-import PlutusLedgerApi.V1 qualified as V1
-import PlutusLedgerApi.V2 qualified as V2
+import PlutusCore.Pretty
+import Prettyprinter
 import PyF (fmt)
 import System.Directory.Extra (listFiles)
 import System.Exit (die)
-import System.FilePath (isExtensionOf, (<.>))
+import System.FilePath (FilePath, isExtensionOf, (<.>))
 import System.IO.Extra (withTempDir)
 import System.Process.Extra (system_)
 
-data TestFailure
-    = InvalidEvaluationResult
-        Text
-        -- ^ Serialised `ScriptEvaluationEvent`
-        ScriptEvaluationResult
-    | InvalidCostParams
-        Text
-        -- ^ Serialised `ScriptEvaluationEvent`
-        CostModelApplyError
-    deriving stock (Show)
+data TestFailure = TestFailure
+  { tfEventsFile :: FilePath,
+    tfErrors     :: NonEmpty UnexpectedEvaluationResult
+  } deriving stock (Show)
+
+instance Pretty TestFailure where
+    pretty TestFailure{..} = nest 2 $ vsep
+     [ "Test Failures in" <+> pretty tfEventsFile,
+       vsep (pretty <$> toList tfErrors)
+     ]
 
 main :: IO ()
 main = do
     opts <- OA.execParser O.parserInfo
-    let extZipped = O.optsEventFileExt opts <.> "bz2"
-    withTempDir $ \dir -> do
-        putStrLn "Downloading event dump files"
-        let s3DownloadCmd :: String
-            s3DownloadCmd = [fmt|{keyId} {secretKey} {region} aws {endpoint} s3 cp {s3Path} {dir} {options}|]
-              where
-                keyId = [fmt|AWS_ACCESS_KEY_ID={O.optsS3AccessKeyId opts}|]
-                secretKey = [fmt|AWS_SECRET_ACCESS_KEY={O.optsS3SecretAccessKey opts}|]
-                region = [fmt|AWS_DEFAULT_REGION={O.optsS3Region opts}|]
-                endpoint = [fmt|--endpoint-url {O.optsS3Endpoint opts}|]
-                s3Path = [fmt|s3://{O.optsS3Bucket opts}/{O.optsS3Prefix opts}|]
-                options = [fmt|--recursive --exclude "*" --include "*.{extZipped}"|]
-        putStrLn $ "Running: " <> s3DownloadCmd
-        -- It would be better to use amazonka, but the version bounds don't quite line up
-        -- (e.g., amazonka-core has aeson <1.6)
-        system_ s3DownloadCmd
-        eventFilesZipped <- filter (extZipped `isExtensionOf`) <$> listFiles dir
-        for_ eventFilesZipped $ \zipped -> do
-            putStrLn $ "Unzipping " <> zipped
-            system_ $ "bunzip2 " <> zipped
-        eventFiles <- filter (O.optsEventFileExt opts `isExtensionOf`) <$> listFiles dir
-        for_ (zip [1 :: Int ..] eventFiles) $ \(i, eventFile) -> do
-            putStrLn [fmt|Processing event file {i}: {eventFile}|]
-            events <- CBOR.readFileDeserialise @[ScriptEvaluationEvent] eventFile
-            mbErrs <- Concurrent.withPool (O.optsConcurrency opts) $ \pool -> do
-                Concurrent.parallel pool $ fmap (evaluate . checkEvaluationEvent) events
-            case catMaybes mbErrs of
-                [] -> putStrLn "All tests passed"
-                errs ->
-                    die
-                        [fmt|
-Script evaluation regression test failed.
-Number of failed test cases: {length errs}
-{intercalate "\n" (show <$> errs)}
+    eventFiles <- filter (O.optsEventFileExt opts `isExtensionOf`) <$> listFiles (O.optsDir opts)
+    mbErrs <- for (zip [1 :: Int ..] eventFiles) $ \(i, eventFile) -> do
+        putStrLn [fmt|Processing event file {i}: {eventFile}|]
+        events <- CBOR.readFileDeserialise @ScriptEvaluationEvents eventFile
+        ctxV1 <- either invalidCostParams pure $
+           mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
+        ctxV2 <- either invalidCostParams pure $
+           mkContext V1.mkEvaluationContext (eventsCostParamsV1 events)
+        mbErrs <- Concurrent.withPool (O.optsConcurrency opts) $ \pool -> do
+            Concurrent.parallel pool $ fmap (evaluate . runTest ctxV1 ctxV2) (toList (eventsEvents events))
+        case nonEmpty (catMaybes mbErrs) of
+                Nothing -> do
+                    putStrLn "All tests passed"
+                    pure Nothing
+                Just errs -> do
+                    putStrLn [fmt|
+Some tests failed. Number of failures: {length errs}
+{render . indent 2 . vsep $ pretty <$> toList errs :: Text}
+|]
+                    pure $ Just (TestFailure eventFile errs)
+    whenJust (nonEmpty (catMaybes mbErrs)) $ \errs -> die [fmt|
+Total number of failed tests: {sum . fmap length $ tfErrors <$> errs}
+Number of event files with failed tests: {length errs}
+{render . indent 2 . vsep $ pretty <$> toList errs :: Text}
 |]
 
-checkEvaluationEvent :: ScriptEvaluationEvent -> Maybe TestFailure
-checkEvaluationEvent ev = case ev of
-    PlutusV1Event ScriptEvaluationData{..} expected ->
-        case V1.mkEvaluationContext dataCostParams of
-            Left err -> Just $ InvalidCostParams ev' err
-            Right ctx ->
-                let (_, actual) =
-                        V1.evaluateScriptRestricting
-                            dataProtocolVersion
-                            V1.Quiet
-                            ctx
-                            dataBudget
-                            dataScript
-                            dataInputs
-                 in case (expected, actual) of
-                        (ScriptEvaluationSuccess, Left _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationFailure
-                        (ScriptEvaluationFailure, Right _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationSuccess
-                        _ -> Nothing
-    PlutusV2Event ScriptEvaluationData{..} expected ->
-        case V2.mkEvaluationContext dataCostParams of
-            Left err -> Just $ InvalidCostParams ev' err
-            Right ctx ->
-                let (_, actual) =
-                        V2.evaluateScriptRestricting
-                            dataProtocolVersion
-                            V2.Quiet
-                            ctx
-                            dataBudget
-                            dataScript
-                            dataInputs
-                 in case (expected, actual) of
-                        (ScriptEvaluationSuccess, Left _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationFailure
-                        (ScriptEvaluationFailure, Right _) ->
-                            Just $ InvalidEvaluationResult ev' ScriptEvaluationSuccess
-                        _ -> Nothing
-  where
-    ev' :: Text
-    ev' = Text.decodeLatin1 . B16.encode . Lazy.toStrict $ CBOR.serialise ev
+--                     die
+--                         [fmt|
+-- Script evaluation regression test failed.
+-- Number of failed test cases: {length errs}
+-- |]
+-- {intercalate "\n" (show <$> errs)}
+    where
+        runTest :: Maybe EvaluationContext ->
+              Maybe EvaluationContext -> ScriptEvaluationEvent -> Maybe UnexpectedEvaluationResult
+        runTest ctxV1 ctxV2 event = case event of
+            PlutusV1Event{} -> checkEvaluationEvent (fromJust ctxV1) [] event
+            PlutusV2Event{} -> checkEvaluationEvent (fromJust ctxV2) [] event
+
+        mkContext f = \case
+            Nothing         -> Right Nothing
+            Just costParams -> Just <$> f costParams
+
+        invalidCostParams err = die $ "Invalid cost parameters: " <> show err
